4bcfc56ff5e146aebc9e0bd84050fc2e9949d733
Gary Hull <gary.hull@digital.hmrc.gov.uk>
2016-06-14 21:18:42 +0100

    reactored whole repo. added plugins and added controller packages etc..

--- controllers/TestController.scala
+++ controllers/TestController.scala
@@ -1,15 +1,15 @@
-package uk.gov.hmrc.investmenttaxreliefsubmission.controllers
+package controllers
 
 import uk.gov.hmrc.play.microservice.controller.BaseController
 import uk.gov.hmrc.play.http.logging.MdcLoggingExecutionContext._
 import play.api.mvc._
 import scala.concurrent.Future
 
-object MicroserviceHelloWorld extends MicroserviceHelloWorld
+object TestController extends TestController
 
-trait MicroserviceHelloWorld extends BaseController {
+trait TestController extends BaseController {
 
-	def hello() = Action.async { implicit request =>
+	def hello(): Action[AnyContent] = Action.async { implicit request =>
 		Future.successful(Ok("Hello world"))
 	}
 }
